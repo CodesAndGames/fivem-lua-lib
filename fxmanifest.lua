ae4cb4fb3c727d78097cb1dfbb4f0d2ebddd996b
JaxDanger <58536302+Jax-Danger@users.noreply.github.com>
2025-07-28 23:13:55 -0400

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -7,5 +7,10 @@ version "0.0.1" -- last modified: 28/07/2025
 
 
 shared_script "fivem-library.lua" -- Shared script for both client and server
-server_script "example.lua" -- example server script utilizing the library
-client_script "example.lua" -- example client script utilizing the library
+server_script "server-example.lua" -- example server script utilizing the library
+client_script "client-example.lua" -- example client script utilizing the library
+
+escrow_ignore {
+  "server-example.lua",
+  "client-example.lua"
+}
