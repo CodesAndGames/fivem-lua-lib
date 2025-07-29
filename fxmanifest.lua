94b032136f61d40be8b1f12432a564398f8d7cdf
JaxDanger <58536302+Jax-Danger@users.noreply.github.com>
2025-07-29 00:09:12 -0400

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,6 +1,6 @@
 fx_version "cerulean"
 game "gta5"
-
+lua54 "yes"
 author "Jax Danger/ CodesAndGames"
 description "A FiveM Lua Library meant to make FiveM development easier."
 version "0.0.1" -- last modified: 28/07/2025
